2352a392e0e61ddde7bae8f9e8e1b9e2552102d0
nikkymen <nikkymen@gmail.com>
2018-02-01 22:11:01 +0300

    travis edit

--- test/test_lib.cpp
+++ test/test_lib.cpp
@@ -8,3 +8,10 @@ TEST_CASE( "test_1" )
 
     REQUIRE( result == "hello world" );
 }
+
+TEST_CASE( "test_2" )
+{
+    std::string result = test::test_function();
+
+    REQUIRE( result == "hello world 2" );
+}
